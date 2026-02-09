1a86fa28c57d2f2379428a1656c68792809d5579
rishisharma08 <>
2026-02-08 22:53:49 -0500

    add hover state on buttons

--- src/components/general/Button.tsx
+++ src/components/general/Button.tsx
@@ -4,7 +4,7 @@ function Button( props: ButtonHTMLAttributes<HTMLButtonElement> ){
   const {className = "", ...others} = props;
   return <button
     type="button"
-    className={`bg-gray-900 rounded px-3 py-2 font-mono text-sm font-normal text-white mt-2 uppercase cursor-pointer tracking-normal ${className}`}
+    className={`bg-gray-900 hover:bg-gray-700 transition-colors duration-300 rounded px-3 py-2 font-mono text-sm font-normal text-white mt-2 uppercase cursor-pointer tracking-normal ${className}`}
     {...others}
   />;
 }
