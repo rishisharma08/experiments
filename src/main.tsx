9858b7ca702a60bdc02c7aff4bd65961fdf3cc72
rishisharma08 <>
2026-02-15 01:19:09 -0500

    change to hash routing because of github pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider, createBrowserRouter, type RouteObject } from "react-router";
+import { RouterProvider, createHashRouter, type RouteObject } from "react-router";
 import routes from './routeConfig.tsx';
 import './index.css';
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   routes as RouteObject[],
   {
-    basename: import.meta.env.BASE_URL
+    // basename: import.meta.env.BASE_URL //not needed for hash router but needed for browser router
   }
 );
 
